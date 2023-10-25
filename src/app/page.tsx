a3199fcb4e2728f29bc2ac74b5ae9067a7498444
yziori <ms6rx7861@gmail.com>
2023-10-26 02:01:11 +0900

    feat: topページ仮実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
+import React from 'react'
+import background_img from '../../public/top-page-background.jpeg'
+import Image from 'next/image'
+
 const Home = () => {
-  return <button className='btn'>Button</button>
+  return (
+    <div className='relative h-screen w-full overflow-hidden'>
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      >
+        <Image src={background_img} alt='a bird' layout='fill' objectFit='cover' />
+        <div className='absolute inset-0 flex flex-col items-center justify-center bg-black/50 text-center'>
+          <div className='text-5xl font-bold text-white'>WELCOME TO</div>
+          <div className='text-8xl font-bold text-white'>PLATEAU</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
